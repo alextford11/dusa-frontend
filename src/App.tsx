25155ade846d3b18a1373ec0be2bba8bf4cc2035
Alex Ford <a.ford@novafutur.com>
2023-12-27 17:18:08 +0000

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import React from "react";
 import Dashboard from "components/Dashboard";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
-const App: React.FC = () => <Dashboard />;
+const App: React.FC = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Dashboard />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
 
 export default App;
